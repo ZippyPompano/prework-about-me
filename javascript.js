97ff23d073a7ff8c31458e184bee58e47b74f1a2
Preston Weber <preston159@gmail.com>
2019-08-05 15:20:02 -0700

    Adding the javascript portion to my first javascript html

---
+++ javascript.js
@@ -0,0 +1,18 @@
+document.getElementById("button1").addEventListener("click", function(){
+    
+    document.getElementById("box").style.height = "250px";
+    document.getElementById("box").style.width = "250px";
+})
+
+document.getElementById("button2")addEventListener("click", function(){
+    
+    document.getElementById("box").style.background-color = "blue";
+})
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity = ".5";
+})
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box");
+})
